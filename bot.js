ec57c2031535f63a9bd056a2006b15d497501a7c
Rejectt <45071435+Rejectt@users.noreply.github.com>
2019-01-05 03:37:54 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -17,12 +17,205 @@ client.on('ready', () => {
   console.log('╚[════════════]╝')
   console.log('')
   console.log('')
+});
 
-  
-  
 
+client.on('guildMemberAdd', member => {
+  var embed = new Discord.RichEmbed()
+  .setTitle(" 😃 عضو جديد دخل السيرفر")
+  .setColor("RANDOM")
+  .addField("اسم العضو",`${member}`)
+  .addField("ايدي العضو",`${member.id}`)
+  .addField("رقم العضو",`${member.guild.memberCount}`)
+  .setThumbnail("http://www.ymcaswkansas.org/sites/ymcaswkansas.org/files/civicrm/friend.png")
+  var channel =member.guild.channels.find('name', 'bloody')
+if (!channel) return;
+      channel.send({embed : embed});
+});
+
+client.on('guildMemberRemove', member => {
+  var embed = new Discord.RichEmbed()
+  .setTitle("عضو غادر السيرفر")
+  .setColor("RANDOM")
+  .addField("اسم العضو",`${member}`)
+  .addField("ايدي العضو",`${member.id}`)
+  .addField("تبقى",`${member.guild.memberCount}`)
+  .setThumbnail("https://cdn.onlinewebfonts.com/svg/img_948.png")
+  var channel =member.guild.channels.find('name', 'bloody')
+  if (!channel) return;
+        channel.send({embed : embed});
+  });
+
+client.on("message", (message) => {
+   if (message.content.startsWith("$new")) {
+        const reason = message.content.split(" ").slice(1).join(" ");     
+        if (!message.guild.roles.exists("name", "Support Team")) return message.channel.send(`This server doesn't have a \`Support Team\` role made, so the ticket won't be opened.\nIf you are an administrator, make one with that name exactly and give it to users that should be able to see tickets.`);
+        if (message.guild.channels.exists("name", "ticket-{message.author.id}" + message.author.id)) return message.channel.send(`You already have a ticket open.`);    
+        message.guild.createChannel(`ticket-${message.author.username}`, "text").then(c => {
+            let role = message.guild.roles.find("name", "Support Team");
+            let role2 = message.guild.roles.find("name", "@everyone");
+            c.overwritePermissions(role, {
+                SEND_MESSAGES: true,
+                READ_MESSAGES: true
+            });    
+            c.overwritePermissions(role2, {
+                SEND_MESSAGES: false,
+                READ_MESSAGES: false
+            });
+            c.overwritePermissions(message.author, {
+                SEND_MESSAGES: true,
+                READ_MESSAGES: true
+            });
+            message.channel.send(`:white_check_mark: **تم إنشاء تذكرتك ، #${c.name}.**`);
+            const embed = new Discord.RichEmbed()
+                .setColor(0xCF40FA)
+                .addField(`مرحباّ ${message.author.username}!`, `يرجى محاولة شرح سبب فتح هذه التذكرة بأكبر قدر ممكن من التفاصيل. سيكون فريق الدعم لدينا قريبا للمساعدة.`)
+                .setTimestamp();
+            c.send({
+                embed: embed
+            });
+        }).catch(console.error);
+    }
+ 
+ 
+  if (message.content.startsWith("$close")) {
+        if (!message.channel.name.startsWith(`ticket-`)) return message.channel.send(`You can't use the close command outside of a ticket channel.`);
+ 
+        message.channel.send(`هل أنت متأكد؟ بعد التأكيد ، لا يمكنك عكس هذا الإجراء!\n للتأكيد ، اكتب\`*confirm\`. سيؤدي ذلك إلى مهلة زمنية في غضون 10 ثوانٍ وإلغائها`)
+            .then((m) => {
+                message.channel.awaitMessages(response => response.content === '$confirm', {
+                        max: 1,
+                        time: 10000,
+                        errors: ['time'],
+                    })   
+                    .then((collected) => {
+                        message.channel.delete();
+                    })    
+                    .catch(() => {
+                        m.edit('Ticket close timed out, the ticket was not closed.').then(m2 => {
+                            m2.delete();
+                        }, 3000);
+                    });
+            });
+    }
+ 
+});
+
+client.on('message',async msg => {
+  var p = "$";//Toxic Codes
+  if(msg.content.startsWith(p + "setuser")) {
+  if(!msg.guild.member(msg.author).hasPermissions('MANAGE_CHANNELS')) return msg.reply('❌ **ليس لديك صلاحيه**')
+  if(!msg.guild.member(client.user).hasPermissions(['MANAGE_CHANNELS'])) return msg.reply('❌ **البوت لا يمتلك صلاحية**')
+  msg.guild.createChannel(`Members : ◤ → ${client.users.size} ← ◢` , 'voice').then(time => {
+    });/
+
+  }
+ 
+});
+
+client.on('message', message => {
+             if (!message.channel.guild) return;
+      if (message.author.bot) return;
+
+  if (!message.content.startsWith(prefix)) return;
+  let command = message.content.split(" ")[0];
+  command = command.slice(prefix.length);
+  let args = message.content.split(" ").slice(1);
+  
+  if (command === 'invites') {
+    message.guild.fetchInvites().then(invs => {
+      let member = client.guilds.get(message.guild.id).members.get(message.author.id);
+      let personalInvites = invs.filter(i => i.inviter.id === message.author.id);
+      let inviteCount = personalInvites.reduce((p, v) => v.uses + p, 0);
+    return message.reply(`**${inviteCount}: عدد الاشخاص الذي دعوتهم هو**`)
 
 });
+}});
+
+client.on('message', message => {
+        if (message.content.toLowerCase() === prefix + "بوت") {
+            if(!message.channel.guild) return;
+        let embed = new Discord.RichEmbed()
+        .setTitle(`:small_orange_diamond: اضغط هنا لتضم البوت عندك في السيرفر. `)
+        .setURL(`https://discordapp.com/oauth2/authorize?client_id=${client.user.id}&scope=bot&permissions=8`)
+     message.channel.sendEmbed(embed);
+       }
+   });
+
+client.on('message' , message => {
+ if(message.content.split(' ')[0].toLowerCase() == prefix + 'id') {
+     const millis = new Date().getTime() - message.guild.createdAt.getTime();
+    const now = new Date();
+    const createdAt = millis / 1000 / 60 / 60 / 24;
+    if(!message.channel.guild) return message.reply(' Error : \` Guild Command \`');
+    let user = message.mentions.users.first() || message.author;
+    message.delete();
+   
+var men = message.mentions.users.first();
+ var heg;
+ if(men) {
+     heg = men
+ } else {
+     heg = message.author
+ }
+var mentionned = message.mentions.members.first();
+  var h;
+ if(mentionned) {
+     h = mentionned
+ } else {
+     h = message.member
+ }
+
+    let game;
+    if (user.presence.game === null) {
+        game = 'None.';
+    } else {
+        game = user.presence.game.name;
+    }
+    let messag;
+    if (user.lastMessage === null) {
+        messag = 'None.';
+    } else {
+        messag = user.lastMessage;
+    }
+    let status;
+    if (user.presence.status === 'online') {
+        status = 'Online';
+    } else if (user.presence.status === 'dnd') {
+        status = 'DND';
+    } else if (user.presence.status === 'idle') {
+        status = 'Idle';
+    } else if (user.presence.status === 'offline') {
+        status = 'Offline';
+    }
+    if (user.presence.status === 'offline') {
+        stat = 0x000000;
+    } else if (user.presence.status === 'online') {
+        stat = 0x00AA4C;
+    } else if (user.presence.status === 'dnd') {
+        stat = 0x9C0000;
+    } else if (user.presence.status === 'idle') {
+        stat = 0xF7C035;
+    }
+    moment.locale('En-ly');
+                    message.guild.fetchInvites().then(invs => {
+      let personalInvites = invs.filter(i => i.inviter.id === message.author.id);
+      let Invites = invs.filter(i => i.inviter.id);
+      let inviteCount = personalInvites.reduce((p, v) => v.uses + p, 0);
+    const embed = new Discord.RichEmbed()
+
+  
+  .addField('Discord Info : ', `Name : ${user.username}\n Discriminator: #${user.discriminator}\nID : ${user.id} \nJoinedDiscord : ${moment(heg.createdTimestamp).fromNow()}\nBot :  ${user.bot}\nPlaying : ${game}\nStatus : ${status}`,true)
+  .addField('Server Info :', `LastMessage : ${messag}\nJoined :  ${moment(h.joinedAt).fromNow()} \n Invites :  ${inviteCount} Invite(s) \nRoles : `+message.guild.members.get(user.id).roles.array(role => role.name).slice(1).join(', '))
+  .setAuthor(`${user.username}`, user.displayAvatarURL)
+  .setColor('#36393e')
+    .setThumbnail(user.displayAvatarURL)
+    message.channel.send({embed})
+  .catch(e => logger.error(e));
+ })
+}
+ });
+
  
  
  
